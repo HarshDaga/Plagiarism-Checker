d0ac3771381be4ca2c01b1ba7b39176d23b0ed3c
DevilDaga <dagaddevil@gmail.com>
2017-04-05 02:18:32 +0530

    Fix certain type of Array Dereference statements not being read

--- FlowGraph/GArrayDereference.cs
+++ FlowGraph/GArrayDereference.cs
@@ -5,17 +5,27 @@
 public class GArrayDereference : GVar
 {
 	public static readonly string pattern = @"^MEM\[base: (?<name>[\w\.]+), offset: (?<offset>\w+)]$";
+	public static readonly string pattern2 = @"^MEM\[(\(.*\))?(?<name>[\w\.]+)\]$";
 
 	public string Offset { get; set; }
 
 	public GArrayDereference ( string str )
 	{
-		var match = Regex.Match ( str, pattern );
-		Name = match.Groups["name"].Value;
-		Offset = match.Groups["offset"].Value;
+		if ( Regex.IsMatch ( str, pattern ) )
+		{
+			var match = Regex.Match ( str, pattern );
+			Name = match.Groups["name"].Value;
+			Offset = match.Groups["offset"].Value;
+		}
+		else if ( Regex.IsMatch ( str, pattern2 ) )
+		{
+			var match = Regex.Match ( str, pattern2 );
+			Name = match.Groups["name"].Value;
+			Offset = "0";
+		}
 	}
 
-	public static bool Matches ( string str ) => Regex.IsMatch ( str, pattern );
+	public static bool Matches ( string str ) => Regex.IsMatch ( str, pattern ) || Regex.IsMatch ( str, pattern2 );
 
 	public override int GetHashCode ( ) => Name.GetHashCode ( );
 
